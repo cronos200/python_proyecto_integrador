87eb608efcd556d8ed4704ab9702a838114a138b
jhon jairo <jhonjdv2022@outlook.com>
2025-05-05 17:49:00 -0500

    cambios en la direccion del archivo CSV y eliminacion de un archivo no encensial

--- pages/analisis_expploratorios.py
+++ pages/analisis_expploratorios.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.header('Proyecto integrador')
-df = pd.read_csv('datasest/Environment_Temperature_change_E_All_Data_NOFLAG.csv', encoding='latin1')
+df = pd.read_csv('static/Environment_Temperature_change_E_All_Data_NOFLAG.csv', encoding='latin1')
 st.text('Daostos del CSV del proyecto')
 st.dataframe(df)
