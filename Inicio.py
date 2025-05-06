b3bcd9ff2c619492faf21e3a8745f68a412d2e4e
jhon jairo <jhonjdv2022@outlook.com>
2025-05-05 19:34:01 -0500

    creacion del README y estilos en el proyecto

--- Inicio.py
+++ Inicio.py
@@ -128,10 +128,10 @@ with col1:
 # Columna derecha: Información del estudiante
 with col2:
     st.markdown('<h2 style="color: #0066cc; margin-top: 0px;">Proyecto integrador</h2>', unsafe_allow_html=True)
-    st.markdown('<p style="margin-top: 10px;">integrantes: <span style="color: #0066cc; font-weight: bold;">jhon jairo devia velasco</span></p>', unsafe_allow_html=True)
+    st.markdown('<p style="margin-top: 10px;">integrantes: <span style="color: #0066cc; font-weight: bold;">Jhon Jairo Devia Velasco, Juan David Berrio y Esteban Gonzalez Ortiz</span></p>', unsafe_allow_html=True)
     st.markdown('<p style="margin-top: 10px;">Programa: <span style="color: #0066cc; font-weight: bold;">Desarrollo de Software</span></p>', unsafe_allow_html=True)
     st.markdown('<p>Semestre: <span style="color: #0066cc; font-weight: bold;">2025-1</span></p>', unsafe_allow_html=True)
-    st.markdown('<p>Repositorio: <a href="https://github.com/username/proyecto-ntp" target="_blank" style="color: #0066cc; font-weight: bold; text-decoration: none;">GitHub</a></p>', unsafe_allow_html=True)
+    st.markdown('<p>Repositorio: <a href="https://github.com/cronos200/python_proyecto_integrador.git" target="_blank" style="color: #0066cc; font-weight: bold; text-decoration: none;">GitHub</a></p>', unsafe_allow_html=True)
    
 
 
--- pages/🔍_analisis_expploratorios.py
+++ pages/🔍_analisis_expploratorios.py
@@ -3,5 +3,7 @@ import pandas as pd
 
 st.header('Proyecto integrador')
 df = pd.read_csv('static/Environment_Temperature_change_E_All_Data_NOFLAG.csv', encoding='latin1')
-st.text('Daostos del CSV del proyecto')
+st.text('📁Daostos del CSV del proyecto')
 st.dataframe(df)
+
+st.header('que podemos')
